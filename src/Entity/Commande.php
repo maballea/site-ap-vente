b84393c78daafffb728868901fde4375d852b64d
nohad <nfaouzi.ledantec@gmail.com>
2024-12-03 09:29:32 +0100

    nohad

---
+++ src/Entity/Commande.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $reference = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $dateCreation = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datecreation = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?float $total = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commandes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $client = null;
+
+    /**
+     * @var Collection<int, produit>
+     */
+    #[ORM\ManyToMany(targetEntity: produit::class, inversedBy: 'commandes')]
+    private Collection $produits;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresseLivraison = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datelivraisonEstimee = null;
+
+    public function __construct()
+    {
+        $this->produits = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): static
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?string
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(string $dateCreation): static
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getTotal(): ?float
+    {
+        return $this->total;
+    }
+
+    public function setTotal(float $total): static
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function getClient(): ?User
+    {
+        return $this->client;
+    }
+
+    public function setClient(?User $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, produit>
+     */
+    public function getProduits(): Collection
+    {
+        return $this->produits;
+    }
+
+    public function addProduit(produit $produit): static
+    {
+        if (!$this->produits->contains($produit)) {
+            $this->produits->add($produit);
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(produit $produit): static
+    {
+        $this->produits->removeElement($produit);
+
+        return $this;
+    }
+
+    public function getAdresseLivraison(): ?string
+    {
+        return $this->adresseLivraison;
+    }
+
+    public function setAdresseLivraison(string $adresseLivraison): static
+    {
+        $this->adresseLivraison = $adresseLivraison;
+
+        return $this;
+    }
+
+    public function getDatelivraisonEstimee(): ?\DateTimeInterface
+    {
+        return $this->datelivraisonEstimee;
+    }
+
+    public function setDatelivraisonEstimee(\DateTimeInterface $datelivraisonEstimee): static
+    {
+        $this->datelivraisonEstimee = $datelivraisonEstimee;
+
+        return $this;
+    }
+}
