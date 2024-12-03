85f4127dd9e7038504f7226a05fff82985c4cb6e
ronan-alt <ridel.ronan@gmail.com>
2024-12-03 08:15:12 +0100

    correction bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -13,11 +13,8 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-HEAD
-HEAD
+
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-c847b097313442a1d8512f8f8dd35a0bd66184dd
-
-fdbf497cb488cfaf4d8490145feb6ba3016cb160];
+c847b097313442a1d8512f8f8dd35a0bd66184dd];
